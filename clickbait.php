a81f5e2ae0053527e1ae97289d3e66129d4d9da1
MelinaRentzi <r.melina_92@hotmail.com>
2018-09-26 20:04:53 +0300

    fixed some issues and comments

--- clickbait.php
+++ clickbait.php
@@ -1,25 +1,24 @@
 <?php
-    define( "TITLE", "Honest Clickbait Headlines" );
+    define( "TITLE", "Honest Click Bait Headlines" );
     include("functions.php");
-    if(isset($_POST["fix_submit"])) {
-        // class the function
+
+    if( isset( $_POST["fix_submit"] ) ) {
+        // call function
         checkForClickBait();
     }
-     ?>
+?>
 
-    <!DOCTYPE html>
+<!DOCTYPE html>
 
-    <html>
+<html>
 
     <head>
-
+        
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>
-            <?php echo TITLE; ?>
-        </title>
+        <title><?php echo TITLE; ?></title>
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
@@ -31,36 +30,68 @@
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
     </head>
-
+    
     <body>
         <div class="container">
-            <h1>
-                <?php echo TITLE; ?>
-            </h1>
+            <h1><?php echo TITLE; ?></h1>
             <p class="lead">Hate click bait? Turn those annoying headlines into realistic and honest ones using this simple program.</p>
-
+            
             <div class="row">
                 <form class="col-sm-8 col-sm-offset-2" action="" method="post">
-                    <textarea placeholder="Paste clickbait headline here" class="form-control input-lg" name="clickbait_headline"></textarea><br>
+                    <textarea placeholder="Paste click bait headline here" class="form-control input-lg" name="clickbait_headline"></textarea><br>
                     <button type="submit" class="btn btn-primary btn-lg pull-right" name="fix_submit">Make honest!</button>
                 </form>
             </div>
-
+            
             <?php
-                if(isset($_POST["fix_submit"])){
-                    // get the first variable from the array and assign it to a variable named $clickBait (new var)
-                    $clickBait = checkForClickBait()[0];
-                    // get the second variable from the array and assign it to a variable named $honestHeadline (new var)
-                    $honestHeadline = checkForClickBait()[1];
-                    
-                    // call function and pass 2 args (the new vars from the previous lines) in the function
-                    displayHonestHeadline($clickBait, $honestHeadline);
-                }
+            if ( isset( $_POST["fix_submit"] ) ) {
+                
+                // get first value in array returned by checkForClickBait() function
+                // store it in a variable
+                $clickBait = checkForClickBait()[0];
+                
+                // get second value in array returned by checkForClickBait() function
+                // store it in a variable
+                $honestHeadline = checkForClickBait()[1];
+                
+                // call function
+                // pass two arguments in the function
+                displayHonestHeadline( $clickBait, $honestHeadline );
+            }
             ?>
 
         </div>
         <!-- Bootstrap JS -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     </body>
+</html>
 
-    </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
